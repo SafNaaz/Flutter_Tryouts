c5d3f2117240be7bdb886f82740fcddfc5818544
SAFNAS <iamsafnas@gmail.com>
2020-06-18 21:54:08 +0530

    basic styling of texts

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,69 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ninja ID Card'),
-      ),
-    );
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Ninja ID Card'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'name',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Safnas',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'CURRENT PROGRAMMER LEVEL',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                '6',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(Icons.email, color: Colors.grey[400]),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('iamsafnas@gmail.com',
+                      style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 18.0,
+                          letterSpacing: 1.0))
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
-
