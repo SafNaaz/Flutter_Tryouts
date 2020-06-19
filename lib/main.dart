96a46d7f6996d4d76cc066339a30f3b941dc1f1c
SAFNAS <iamsafnas@gmail.com>
2020-06-19 12:26:28 +0530

    QuoteList Initial Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: NinjaCard(),
-  ));
+  runApp(MaterialApp(home: QuoteList()));
 }
 
-class NinjaCard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _NinjaCardState createState() => _NinjaCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
-  int ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: AppBar(
-          title: Text('Ninja ID Card'),
-          centerTitle: true,
-          backgroundColor: Colors.grey[850],
-          elevation: 0.0,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              ninjaLevel += 1;
-            });
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.grey[800],
-        ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/Safnas.jpg'),
-                    radius: 40.0),
-              ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey[800],
-              ),
-              Text(
-                'name',
-                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                'Safnas',
-                style: TextStyle(
-                    color: Colors.amberAccent[200],
-                    letterSpacing: 2.0,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Text(
-                'CURRENT PROGRAMMER LEVEL',
-                style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                '$ninjaLevel',
-                style: TextStyle(
-                    color: Colors.amberAccent[200],
-                    letterSpacing: 2.0,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.email, color: Colors.grey[400]),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('iamsafnas@gmail.com',
-                      style: TextStyle(
-                          color: Colors.grey[400],
-                          fontSize: 18.0,
-                          letterSpacing: 1.0))
-                ],
-              )
-            ],
-          ),
-        ));
+    return Container();
   }
 }
