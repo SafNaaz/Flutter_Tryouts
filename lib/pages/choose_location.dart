2eba6ab7e7faea460d791ac1bb1fe1abe53f3612
SAFNAS <iamsafnas@gmail.com>
2020-06-20 15:25:35 +0530

    order of lifecycle methods

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,7 +7,14 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
   @override
+  void initState() {
+    super.initState();
+    print('initState run');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build run');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
