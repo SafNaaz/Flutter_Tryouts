824477cbc88ef9eee3739959cd5c94fbb2236dde
SAFNAS <iamsafnas@gmail.com>
2020-06-20 14:39:30 +0530

    added edit location button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,16 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Text('home screen')));
+    return Scaffold(
+        body: SafeArea(
+            child: Column(
+      children: <Widget>[
+        FlatButton.icon(
+          icon: Icon(Icons.edit_location),
+          label: Text('Edit Location'),
+          onPressed: () {},
+        )
+      ],
+    )));
   }
 }
