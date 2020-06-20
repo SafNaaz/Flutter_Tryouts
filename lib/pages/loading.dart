8e0c530ed81a6a04ee63e890d96daa6d899d4241
SAFNAS <iamsafnas@gmail.com>
2020-06-20 16:42:02 +0530

    convert and get data from map

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -10,14 +11,15 @@ class _LoadingState extends State<Loading> {
   void getData() async {
     Response response =
         await get('https://jsonplaceholder.typicode.com/todos/1');
-    print(response.body);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['userId']);
   }
 
   @override
   void initState() {
     super.initState();
     getData();
-    print('hello');
   }
 
   @override
