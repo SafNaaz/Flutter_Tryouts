400f5cc2633858edf383986be018580feabff3e2
SAFNAS <iamsafnas@gmail.com>
2020-06-20 14:36:30 +0530

    setup route and initialRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ninjaid/pages/home.dart';
+import 'package:ninjaid/pages/loading.dart';
+import 'package:ninjaid/pages/choose_location.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(MaterialApp(initialRoute: '/home', routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  }));
 }
