3d46f22dfcf7414329b8f501476434cf9f47f586
SAFNAS <iamsafnas@gmail.com>
2020-07-08 18:23:11 +0530

    time in political format using intl

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location; //location name for the UI
@@ -24,7 +25,7 @@ class WorldTime {
           hours: int.parse(offsetHours), minutes: int.parse(offsetMinutes)));
 
       //set time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
       print(e);
       time = 'could not get time data';
