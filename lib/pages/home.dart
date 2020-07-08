f94c3c2911cd212ced0a16f220337b67b7de2d00
SAFNAS <iamsafnas@gmail.com>
2020-07-08 22:25:19 +0530

    added image based on time

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,39 +12,47 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     data = ModalRoute.of(context).settings.arguments;
 
+    // set background
+    String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
+
     return Scaffold(
         body: SafeArea(
-            child: Padding(
-      padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-      child: Column(
-        children: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.edit_location),
-            label: Text('Edit Location'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/location');
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                data['location'],
-                style: TextStyle(fontSize: 28, letterSpacing: 2),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            data['time'],
-            style: TextStyle(fontSize: 66),
-          )
-        ],
+            child: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/$bgImage'), fit: BoxFit.cover)),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+        child: Column(
+          children: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.edit_location),
+              label: Text('Edit Location'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/location');
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  data['location'],
+                  style: TextStyle(fontSize: 28, letterSpacing: 2),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              data['time'],
+              style: TextStyle(fontSize: 66),
+            )
+          ],
+        ),
       ),
     )));
   }
